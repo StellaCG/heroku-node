21734e6aaf9d426d50225e03c7c81b689e2ec8d6
Stella <stella@pop-os.localdomain>
2020-07-30 16:36:50 -0400

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var tmi = require('tmi.js');
 var app = express();
 var port = process.env.PORT || 8000;
 
@@ -12,3 +13,23 @@ app.get('/', function(req,res) {
 app.listen(port, function() {
     console.log(`Hosting on ${port}`);
 })
+
+const client = new tmi.Client({
+	connection: {
+		secure: true,
+		reconnect: true
+	},
+	channels: [ 'thylacineFGC' ]
+});
+
+client.connect();
+
+
+client.on('message', (channel, tags, message, self) => {
+    if (self) return true;
+    const { username } = tags;
+    
+    console.log(username);
+	console.log(`${tags['display-name']}: ${message}`);
+});
+		
