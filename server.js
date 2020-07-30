b1a3cf7904d89485d601cf0c6993760682ed3bdb
Stella <stella@pop-os.localdomain>
2020-07-30 17:35:17 -0400

    implemented potato

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-var express = require('express');
-var tmi = require('tmi.js');
-var app = express();
-var port = process.env.PORT || 8000;
+const express = require('express');
+const tmi = require('tmi.js');
+
+const app = express();
+const port = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
-app.get('/', function(req,res) {
-    res.render('index');
+app.get('/', (req, res) => {
+  res.render('index', {
+	  var1: 'potato',
+  });
 });
 
-app.listen(port, function() {
-    console.log(`Hosting on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Hosting on ${port}`);
+});
 
 const client = new tmi.Client({
-	connection: {
-		secure: true,
-		reconnect: true
-	},
-	channels: [ 'thylacineFGC' ]
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  channels: ['thylacineFGC'],
 });
 
 client.connect();
 
-
 client.on('message', (channel, tags, message, self) => {
-    if (self) return true;
-    const { username } = tags;
-    
-    console.log(username);
-	console.log(`${tags['display-name']}: ${message}`);
+  if (self) return true;
+  const { username } = tags;
+
+  console.log(username);
+  console.log(`${tags['display-name']}: ${message}`);
 });
-		
